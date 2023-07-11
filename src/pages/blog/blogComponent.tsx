55470887778fe4fa19afda4da1c2b9b3442aed63
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-07-11 09:15:39 -0300

    Consegui
    Cara, sou brabo

--- src/pages/blog/blogComponent.tsx
+++ src/pages/blog/blogComponent.tsx
@@ -2,13 +2,12 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { blogList } from '../../config/data';
 
-
 export const BlogComponent = () => {
-  const { id } = useParams();
+  const { id }: any = useParams();
   const [blog, setBlog] = useState();
 
   useEffect(() => {
-    let blog = blogList.find((blog) => blog.id === parseInt(id);
+    let blog: any = blogList.find((blog) => blog.id === parseInt(id));
 
     if (blog) {
       setBlog(blog);
